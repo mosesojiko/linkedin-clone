4bd56e0f7edfc2c3705c771a17a6a48491a8459f
Moses Ojiko <mosesojiko999@gmail.com>
2021-09-22 07:57:26 -0700

    Post image to firebase

--- linkedin-clone/src/steps/step32.js
+++ linkedin-clone/src/steps/step32.js
@@ -1,3 +1,98 @@
+//Upload image to firebase store
+
+/* 
+1. actions/index.js, add a new function postArticleApi
+2. Add the function in PostModal.js
+3. Add a postArticle function in PostModal.js
+4. Add an onClick to the PostButton
+5. Add postArticle to dispatch
+6. Go to firebase.com, firestore database and create a database
+*/
+
+
+// modify actions/index.js
+import { auth, provider, storage } from '../firebase';
+import db from '../firebase';
+import { SET_USER } from './actionType';
+
+
+export const setUser = (payload) => ({
+    type: SET_USER,
+    user: payload,
+})
+
+
+export function signInApi() {
+    return (dispatch) => {
+        auth.signInWithPopup(provider)
+        .then((payload) => {
+            dispatch(setUser(payload.user))
+        })
+        .catch((error) => alert(error.message))
+    }
+}
+
+//function to store the user info
+export function getUserAuth() {
+    return (dispatch) => {
+        auth.onAuthStateChanged(async (user) =>{
+            if(user) {
+                dispatch(setUser(user))
+            }
+        })
+    }
+}
+
+// signout function
+export function signOutApi() {
+    return (dispatch) => {
+        auth.signOut().then(()=>{
+            dispatch(setUser(null))
+        }).catch(error =>{
+            console.log(error)
+        })
+    }
+}
+
+//post images to firebase
+export function postArticleApi(payload) {
+    return (dispatch) => {
+        if(payload.image !== "") {
+            const upload = storage
+            .ref(`/images/${payload.image.name}`)
+            .put(payload.image);
+            upload.on('state_changed',
+            (snapshot) => {
+                const progress = ( (snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+                console.log(`Progress: ${progress}%`);
+                if(snapshot.state === 'RUNNING'){
+                    console.log(`Progress: ${progress}%`);
+                }
+            }, 
+            error => {console.log(error.code)},
+            async () => {
+                const downloadURL = await upload.snapshot.ref.getDownloadURL();
+                db.collection('articles').add({
+                    actor: {
+                        description: payload.user.email,
+                        title: payload.user.displayName,
+                        date: payload.timestamp,
+                        image: payload.user.photoURL,
+                    },
+                    video: payload.video,
+                    sharedImg: downloadURL,
+                    comments: 0,
+                    description: payload.description
+                });
+            }
+            )
+            
+        }
+    }
+}
+
+
+// modify PostModal.js
 import { useState } from 'react';
 import styled from 'styled-components';
 import ReactPlayer from 'react-player';
