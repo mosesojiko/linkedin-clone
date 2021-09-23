7ef0e2e48609f69024bff34e1b079801fac08a53
Moses Ojiko <mosesojiko999@gmail.com>
2021-09-23 16:59:58 -0700

    Deploy

---
+++ linkedin-clone/src/steps/step39.js
@@ -0,0 +1,10 @@
+//Deployment of the app
+
+/*
+1. npm i -g firebase-tools
+2. firebase init
+Follow the prompts, select "hosting", "use an existing project", select your project, type "build" where applicable
+3. npm run build
+4. firebase deploy
+
+*/
